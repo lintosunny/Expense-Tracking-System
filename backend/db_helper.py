2cc0cf87f05fa9a289b88d6495dfe04006db98c5
lintosunny <lintosunny111@gmail.com>
2025-05-11 12:41:00 +0530

    log file added

--- backend/db_helper.py
+++ backend/db_helper.py
@@ -1,7 +1,12 @@
 import mysql.connector
 from contextlib import contextmanager
 from dotenv import load_dotenv
+from logger import setup_logger
 import os
+
+
+logger = setup_logger('db_helper')
+
 
 load_dotenv(override=True)
 
@@ -28,6 +33,7 @@ def get_db_cursor(commit=False):
 
 
 def fetch_expenses_for_date(expense_date):
+    logger.info(f"fetch_expense_for_date() called with {expense_date}")
     with get_db_cursor() as cursor:
         cursor.execute("SELECT * from expenses WHERE expense_date = %s", (expense_date,))
         expenses = cursor.fetchall()
@@ -35,11 +41,13 @@ def fetch_expenses_for_date(expense_date):
     
 
 def delete_expense_for_date(expense_date):
+    logger.info(f"delete_expense_for_date() called with {expense_date}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute("DELETE FROM expenses WHERE expense_date = %s", (expense_date,))
 
 
 def insert_expense(expense_date, amount, category, notes):
+    logger.info(f"insert_expense() called with {expense_date}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute(
             "INSERT INTO expenses (expense_date, amount, category, notes) VALUES (%s, %s, %s, %s)",
@@ -48,6 +56,7 @@ def insert_expense(expense_date, amount, category, notes):
 
 
 def fetch_expense_summary(start_date, end_date):
+    logger.info(f"fetch_expense_summary() called with start: {start_date} and end: {end_date}")
     with get_db_cursor() as cursor:
         cursor.execute(
             '''SELECT category, SUM(amount) total_amount
@@ -61,9 +70,9 @@ def fetch_expense_summary(start_date, end_date):
 
 
 if __name__ == "__main__":
-    # expenses = fetch_expenses_for_date("2024-08-01")
-    # print(expenses)
-    # insert_expense("2024-08-01", 50, "Food", "Puffs")
-    # delete_expense_for_date("2024-08-01")
+    expenses = fetch_expenses_for_date("2024-08-01")
+    print(expenses)
+    insert_expense("2024-08-01", 50, "Food", "Puffs")
+    delete_expense_for_date("2024-08-01")
     summary = fetch_expense_summary("2024-08-03", "2024-08-04")
     print(summary)
